f08c7acbc08f5956e87fa01cc3b0da2ffd497651
Pedro Nieto <pedronieto.2005@gmail.com>
2024-01-30 17:16:16 -0300

    test: added health check test

---
+++ test/e2e/health.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { AppModule } from '@/app.module';
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  describe('/ (GET)', () => {
+    it('should return 200 if all services are up and healthy', async () => {
+      return request(app.getHttpServer()).get('/').expect(200);
+    });
+  });
+});
